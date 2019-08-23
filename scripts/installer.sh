45012be7c04a05e257623e1c850ffaf99f07b83e
David Arribas Hidalgo <45437051+davidahid@users.noreply.github.com>
2019-08-23 22:29:36 +0200

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -4,6 +4,6 @@ cd
 sudo mkdir /etc/netutils/
 sudo cp Network-Utils/scripts/network_utils.sh /etc/netutils/
 sudo cp Network-Utils/scripts/netutils /bin/
-sudo chmod u+x /bin/netutils
+sudo chmod +x /bin/netutils
 cd
 sudo rm -rf Network-Utils/
