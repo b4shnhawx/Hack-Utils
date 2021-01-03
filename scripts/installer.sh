f2bf131f6b4767d5db2f4997e1e9d5ca90989e7d
darribash <45437051+davidahid@users.noreply.github.com>
2021-01-03 01:16:32 +0100

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,7 +3,7 @@
 cd
 sudo mkdir /etc/hackutils/
 sudo cp Hack-Utils/scripts/hack_utils.sh /etc/hackutils/
-sudo cp Hack-Utils/scripts/bl.sh /etc/netutils/
+sudo cp Hack-Utils/scripts/bl.sh /etc/hackutils/
 sudo cp Hack-Utils/scripts/hackutils /bin/
 
 sudo chmod 700 /bin/hackutils
