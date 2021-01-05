33a1dfc9726a12d1cccd3ab0c55f7f499b768fdb
darribash <45437051+davidahid@users.noreply.github.com>
2021-01-05 18:10:29 +0000

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,6 +3,10 @@
 cd
 sudo mkdir /etc/hackutils/
 sudo touch /etc/hackutils/hack_utils.conf
+cat <<EOF > /etc/hackutils/hack_utils.conf
+OVPNS_DIR="$HOME/.secret/ovpns/"
+HTB_DIR="$HOME/HTB/"
+EOF
 sudo cp Hack-Utils/scripts/hack_utils.sh /etc/hackutils/
 sudo cp Hack-Utils/scripts/bl.sh /etc/hackutils/
 sudo cp Hack-Utils/scripts/hackutils /bin/
