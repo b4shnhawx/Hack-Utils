71a302fa99eb8648b20db014ae9941d53c97fdd4
darribash <45437051+davidahid@users.noreply.github.com>
2021-01-09 03:34:50 +0100

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -13,11 +13,7 @@ HTB_OVPN_NAME=YOUR_HTB_NAME.ovpn
 EOF
 sudo cp Hack-Utils/scripts/hack_utils.conf /etc/hackutils/
 
-sudo cp Hack-Utils/scripts/hack_utils.sh /etc/hackutils/
-sudo cp Hack-Utils/scripts/bl.sh /etc/hackutils/
-sudo cp Hack-Utils/scripts/htbMkt.sh /etc/hackutils/
-sudo cp Hack-Utils/scripts/dwa.sh /etc/hackutils/
-sudo cp -r Hack-Utils/scripts/conky/ /etc/hackutils/
+sudo cp -r Hack-Utils/scripts/* /etc/hackutils/
 sudo cp Hack-Utils/scripts/hackutils /bin/
 
 sudo chmod 700 /bin/hackutils
